c2e2bf3fe281c318db77cf56fd9d830f2e40f840
rhit-brelanre <brelanre@rose-hulman.edu>
2024-05-07 16:59:43 -0400

    New file for proxy

---
+++ newproxy.py
@@ -0,0 +1,124 @@
+import socket
+import sys
+import threading
+import traceback
+from urllib.parse import urlparse
+ 
+ 
+class BadRequestException(Exception):
+    pass
+ 
+ 
+class NotImplementedException(Exception):
+    pass
+ 
+ 
+class HttpRequest:
+    def __init__(self, request):
+        split = request.split()
+        self.method = split[0]
+        self.url = urlparse(split[1]).netloc
+ 
+        if ':' in self.url:
+            self.url, self.port = self.url.split(':')
+            self.port = int(self.port)
+        else:
+            self.port = 80
+ 
+        self.http_version = split[2]
+ 
+    @classmethod
+    def verify_valid_http_request(cls, request):
+        # Split the request into lines
+        lines = request.split('\r\n')
+ 
+        # The first line should contain the HTTP method, path, and version
+        first_line_parts = lines[0].split(' ')
+        if len(first_line_parts) != 3:
+            raise BadRequestException('First line should contain the HTTP method, path, and version')
+        method, path, version = first_line_parts
+ 
+        if method not in ['GET']:
+            raise NotImplementedException(f'Method {method} not supported')
+ 
+        # The version should be "HTTP/1.0"
+        #if version != "HTTP/1.0":
+        #    raise NotImplementedException('The proxy only supports HTTP/1.0')
+ 
+        # Check that all headers have the correct format
+        for line in lines[1:]:
+            if line == '':
+                # An empty line indicates the end of the headers
+                break
+            if ':' not in line:
+                # Headers should be of the form "Header-Name: header value"
+                raise BadRequestException('Headers should be in the form \"Header-Name: header value\"')
+            header_name, header_value = line.split(':', 1)
+            if not header_name.strip() or not header_value.strip():
+                # Header names and values should not be empty
+                raise BadRequestException('Header names and values should not be empty')
+ 
+        # The request is valid
+        return True
+ 
+ 
+def handle_client(client_socket):
+    # Receive data from the client
+    try:
+        request_bytes = client_socket.recv(4096) + b'\r\n'
+        request_bytes = request_bytes.decode().replace('HTTP/1.1', 'HTTP/1.0').replace('Connection: keep-alive', 'Connection: close').encode()
+        print(request_bytes.decode())
+        print(request_bytes)
+        HttpRequest.verify_valid_http_request(request_bytes.decode())
+        request = HttpRequest(request_bytes.decode())
+    except BadRequestException:
+        print("Handling bad request")
+        response = b"HTTP/1.0 500 Malformed Request\r\nContent-Type: text/html\r\n\r\n<html><body><h1>500 Malformed Request</h1></body></html>"
+        client_socket.send(response)
+        return
+    except NotImplementedException:
+        print("Handling not implemented")
+        response = b"HTTP/1.0 501 Not Implemented\r\nContent-Type: text/html\r\n\r\n<html><body><h1>501 Not Implemented</h1></body></html>"
+        client_socket.send(response)
+        return
+    except:
+        traceback.print_exc(file=sys.stderr)
+        return
+ 
+    print(request.__dict__)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+    server_socket.connect((request.url, request.port))
+    server_socket.send(request_bytes)
+ 
+    # Receive data from the server
+    response = b''
+    while True:
+        data = server_socket.recv(4096)
+        if not data:
+            break
+        response += data
+ 
+    client_socket.sendall(response)
+ 
+    server_socket.close()
+ 
+ 
+def main():
+    # Create a listening socket
+    proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    proxy_socket.bind(("localhost", 8080))
+    proxy_socket.listen(5)
+ 
+    while True:
+        # Accept a client connection
+        client_socket, client_address = proxy_socket.accept()
+        print("connected")
+ 
+        # Create a new thread to handle the client request
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread.start()
+ 
+ 
+if __name__ == '__main__':
+    main()
--- proxy.py
+++ proxy.py
@@ -17,11 +17,12 @@ class proxy():
             self.your_port = 6789
         else:
             self.your_port = sys.argv[2]
+
+        signal.signal(signal.SIGINT, self.shutdown)
         
 
-    def shutdown(self):
-        # add your signal shutdown code here
-        pass
+    def shutdown(self, signum, frame):
+        self.serverSocket.close()
 
     def getClientName(self, ip_address):
         try:
@@ -31,10 +32,6 @@ class proxy():
             return "No domain name found"
 
     def client_threading(self):
-        signal.signal(signal.SIGINT, self.shutdown)
-
-        self.serverSocket.bind((self.your_ip, self.your_port))
-
         while True:
             # establish the connection
             self.serverSocket.listen(10)
@@ -47,6 +44,7 @@ class proxy():
             d.start()
 
     def main(self, conn, client_address):
+
         # get the request from browser
         from_client = conn.recv(4096)
         request = from_client.decode("utf-8")
@@ -54,63 +52,67 @@ class proxy():
         # parse the first line
         print("from_client: ", from_client)
         print("request: ", request)
-        first_line = request.split('\n')[0]
+        split_request = request.split('\n')
+        first_line = split_request[0]
         print("first line: ", first_line)
 
         # get url
-        url = first_line.split(' ')[1]
-        print("url: ",  url)
+        first_line_split = first_line.split(' ')
+        command = first_line_split[0]
+        if command == "GET":
+            url = first_line_split[1]
+            print("url: ",  url)
 
-        http_pos = url.find("://")
-        if http_pos == -1:
-            temp = url
-        else:
-            temp = url[(http_pos + 3):]
-
-        port_pos = temp.find(":")
-
-        # find end of web server
-        webserver_pos = temp.find("/")
-        if webserver_pos == -1:
-            webserver_pos = len(temp)
-
-        webserver = ""
-        port = -1
-
-        if port_pos == -1 or webserver_pos < port_pos:
-
-            # default port
-            port = 80
-            webserver = temp[:webserver_pos]
-
-        else:  # specific port
-            port = int((temp[(port_pos + 1):])[:webserver_pos - port_pos - 1])
-            webserver = temp[:port_pos]
-
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(100)
-        s.connect((webserver, port))
-        print("request: " + request)
-        s.sendall(from_client)
-        print("sent data")
-
-        while 1:
-            # receive data from web server
-            data = s.recv(4096)
-            print("data received")
-
-            if len(data) > 0:
-                conn.send(data)  # send to browser/client
-
+            http_pos = url.find("://")
+            if http_pos == -1:
+                temp = url
             else:
-                break
+                temp = url[(http_pos + 3):]
+
+            port_pos = temp.find(":")
+
+            # find end of web server
+            webserver_pos = temp.find("/")
+            if webserver_pos == -1:
+                webserver_pos = len(temp)
+                path = ""
+            else:
+                path = temp[(webserver_pos + 1):]
+
+            webserver = ""
+
+            if port_pos == -1 or webserver_pos < port_pos:
+
+                # default port
+                port = 80
+                webserver = temp[:webserver_pos]
+
+            else:  # specific port
+                port = int((temp[(port_pos + 1):])[:webserver_pos - port_pos - 1])
+                webserver = temp[:port_pos]
+
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(100)
+            s.connect((webserver, port))
+            print("webserver: ", webserver)
+
+            # new_command = ("GET /" + path + " HTTP/1.0\r\nHost: " + webserver + "\r\nConnection: close")
+            # print("NEW COMMAND \n", new_command)
+            # s.sendall(new_command.encode("utf-8"))
+            s.sendall(from_client)
+
+            while 1:
+                # receive data from web server
+                data = s.recv(4096)
+                print("data received")
+
+                if len(data) > 0:
+                    conn.send(data)  # send to browser/client
+
+                else:
+                    break
+        else:
+            pass
 
 p = proxy()
 p.client_threading()
-
-def getClientName(self, ip_address):
-    try:
-        hostname = socket.gethostbyaddr(ip_address)[0]
-        return hostname
-    except socket.herror:
-        return "No domain name found"

1ca6ba498fb6ec3bfe71a902d0f7f9e4f4340df4
rhit-brelanre <brelanre@rose-hulman.edu>
2024-05-07 20:19:55 -0400

    Updated newproxy.py

--- newproxy.py
+++ newproxy.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import sys
 from urllib.parse import urlparse
 
 class GETRequest:
@@ -17,7 +18,7 @@ class GETRequest:
         self.http_version = split[2]
 
 
-    def verify_valid_http_request(cls, request):
+    def verify_request(request):
         lines = request.split('\r\n')
         line_1 = lines[0].split(' ')
         if len(line_1) != 3:
@@ -27,50 +28,58 @@ class GETRequest:
             if line == '':
                 break
             if ':' not in line:
-                raise Exception('wrong header format"')
+                raise Exception('wrong header format')
             header_name, header_value = line.split(':', 1)
             if not header_name.strip() or not header_value.strip():
                 raise Exception('empty headers')
         return True
 
 
-def handle_client(client_sock):
-    request_bytes = client_sock.recv(4096) + b'\r\n'
+def handle_client(conn):
+    request_bytes = conn.recv(4096) + b'\r\n'
     request_bytes = request_bytes.decode().replace('HTTP/1.1', 'HTTP/1.0').replace('Connection: keep-alive', 'Connection: close').encode()
-    print(request_bytes.decode())
-    print(request_bytes)
 
-    if not GETRequest.verify_valid_http_request(request_bytes.decode()):
+    if not GETRequest.verify_request(request_bytes.decode()):
         print("Handling bad request")
-        response = b"HTTP/1.0 500 Malformed Request\r\nContent-Type: text/html\r\n\r\n<html><body><h1>500 Malformed Request</h1></body></html>"
-        client_sock.send(response)
+        response = b"HTTP/1.0 400 Bad Request\r\nContent-Type: text/html\r\n\r\n<html><body><h1>400 Bad Request</h1></body></html>"
+        conn.send(response)
         return
+    if (request_bytes.decode().find('GET') == -1):
+        print("Not GET request")
+        response = b"HTTP/1.0 501 Not Implemented\r\nContent-Type: text/html\r\n\r\n<html><body><h1>501 Not Implemented</h1></body></html>"
+        conn.send(response)
+        return
+        
 
     request = GETRequest(request_bytes.decode())
-    print(request.__dict__)
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    server_socket.send(request_bytes)
+    web_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    web_sock.connect((request.url, request.port))
+    web_sock.send(request_bytes)
 
     response = b''
     while True:
-        data = server_socket.recv(4096)
+        data = web_sock.recv(4096)
         if not data:
             break
         response += data
-
-    server_socket.close()
+    
+    conn.sendall(response)
+    web_sock.close()
 
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(("localhost", 8080))
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    if int(len(sys.argv)) != 2:
+        sock.bind(("localhost", 9876))
+    else:
+        sock.bind(("localhost", int(sys.argv[1])))
     sock.listen(5)
 
     while True:
-        client_sock, client_addr = sock.accept()
+        conn, client_addr = sock.accept()
         print("connected")
-        client_thread = threading.Thread(target=handle_client, args=(client_sock,))
+        client_thread = threading.Thread(target=handle_client, args=(conn,))
         client_thread.start()
 
 
